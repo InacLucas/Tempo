9162715628ff8c6f88447123c4045be0ef521bf7
treinamento <treinamento@iMac-de-07hw004658.local>
2019-10-04 09:57:46 -0300

    vc Dia criada, rscunho do layout

---
+++ Tempo/ModelView/DiaViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DiaViewController.swift
+//  Tempo
+//
+//  Created by treinamento on 04/10/19.
+//  Copyright © 2019 EJL. All rights reserved.
+//
+
+import UIKit
+
+class DiaViewController: UIViewController {
+
+    @IBOutlet weak var SemanaLabel: UILabel!
+    @IBOutlet weak var DiaLabel: UILabel!
+    @IBOutlet weak var MesLabel: UILabel!
+    @IBOutlet weak var HoraLabel: UILabel!
+    @IBOutlet weak var SaidaLabel: UILabel!
+    @IBOutlet weak var EntradaLabel: UILabel!
+    @IBOutlet weak var MEButton: UIButton!
+    @IBOutlet weak var MSButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    @IBAction func MEButton(_ sender: Any) {
+    }
+    
+    @IBAction func MSButton(_ sender: Any) {
+    }
+    
+}
